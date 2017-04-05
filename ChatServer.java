23e21e62c8aa2f0f8d961973c6204426f2ddfbde
David Camacho <dac245@users.noreply.github.com>
2017-04-04 20:44:58 -0400

    Style changes for the chat server
    instead of tabs I used 4 spaces to indent my code

--- ChatServer.java
+++ ChatServer.java
@@ -13,71 +13,69 @@ import java.net.*;
  */ 
 
 public class ChatServer{
-		public ArrayList <ServerThread> st = new ArrayList<ServerThread>();	// gets the clients so that we can send the string to each one
+    // gets the clients so that we can send the string to each one
+    public ArrayList<ServerThread> st = new ArrayList<ServerThread>();
 		
-		public static void main(String[] args){
-			new ChatServer();
-		}
+    public static void main(String[] args){
+        new ChatServer();
+    }
 		
-		/**
-		 * The ChatServer constructor gets who is connected and then adds them to the arraylist and then
-		 * we start the thread 
-		 * 
-		 */
-		public ChatServer(){
-			ServerSocket ss = null;
-			
-				try{
-					ss = new ServerSocket(16457);
-					Socket socket = null;
-					
-					while(true){
-						socket = ss.accept();
-						ServerThread threaded = new ServerThread(socket);
-						
-						st.add(threaded);
-						threaded.start();
-					}
-				 }
-				 catch(Exception e){
-						System.out.println("An Error occurred");
-				 }
-		}
+   /**
+     * The ChatServer constructor gets who is connected and then adds them to the arraylist and then
+     * we start the thread.
+     */
+    public ChatServer(){
+        ServerSocket ss = null;
+
+        try{
+            ss = new ServerSocket(16457);
+            Socket socket = null;
+
+            while(true){
+                socket = ss.accept();
+                ServerThread threaded = new ServerThread(socket);
+                st.add(threaded);
+                threaded.start();
+            }
+        }
+        catch(Exception e){
+            System.out.println("An Error occurred");
+        }
+    }
 		
 		
-		/**
-		 * ServerThread has the run method that starts the thread which gets the 
-		 * strings from the clients and send them to everyone connected to the server
-		 * 
-		 */
-		class ServerThread extends Thread{
-				Socket s = null;
-				String clientMsg;
-				PrintWriter pw;
-				BufferedReader br;
-				
-				public ServerThread(Socket _socket){
-					s = _socket;	// gets the socket the other class
-				}
-				
-				public void run(){
-					try{
-							br = new BufferedReader(new InputStreamReader(s.getInputStream()));
-							pw = new PrintWriter(new OutputStreamWriter(s.getOutputStream()));
-							
-							while((clientMsg = br.readLine()) != null){	// loops while it is reading the message
-								
-								System.out.println("Server read: "+ clientMsg);
-								
-								for(ServerThread get : st){	// for loop to iterate through each client and send them the string
-										get.pw.println(clientMsg);	//to clients
-										get.pw.flush();
-								}
-							}
-					}
-					catch(Exception e){
-							System.out.println("Something went wrong");
-					}
-				}
-		}
-	}
+  /**
+    * ServerThread has the run method that starts the thread which gets the 
+    * strings from the clients and send them to everyone connected to the server.
+    */
+    class ServerThread extends Thread{
+        Socket s = null;
+        String clientMsg;
+        PrintWriter pw;
+        BufferedReader br;
+
+        public ServerThread(Socket _socket){
+            s = _socket;    // gets the socket from the other class
+        }
+
+        public void run(){
+            try{
+                br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                pw = new PrintWriter(new OutputStreamWriter(s.getOutputStream()));
+
+                while((clientMsg = br.readLine()) != null){    // loops while it is reading the message
+
+                    System.out.println("Server read: "+ clientMsg);
+
+                    for(ServerThread get : st){    // for loop to iterate through each client and send them the string
+                        get.pw.println(clientMsg);	//to clients
+                        get.pw.flush();
+                    }
+                }
+            }
+            catch(Exception e){
+                System.out.println("Something went wrong");
+            }
+        }
+    }
+}
